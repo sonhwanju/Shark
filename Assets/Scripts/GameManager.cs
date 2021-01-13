b045148b034a555ba70344af5189a45be6bc0ea6
hwanjuson <hwanjuson@gmail.com>
2021-01-13 17:24:53 +0900

    인터페이스 매니저선언

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoSingleton<GameManager>
 {
+    private InterfaceManager interfaceManager;
     void Start()
     {
-        
+        interfaceManager = new InterfaceManager();
     }
 }
