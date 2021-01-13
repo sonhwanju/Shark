312e81edebe2e90294b9946b095ff02a3059a859
Deomyeon <deomyeon@gmail.com>
2021-01-13 17:43:45 +0900

    no message

---
+++ Assets/Scripts/UI/Interface.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interface : MonoBehaviour
+{
+
+    private short instance;
+    private _TyIf type;
+
+
+    public Interface(short instance, _TyIf type)
+    {
+        this.instance = instance;
+        this.type = type;
+    }
+
+    public void PressButton()
+    {
+
+    }
+}
+public enum _TyIf
+{
+    MENU,
+    PARTSHOP,
+    COLLECTION,
+    INVENTORY,
+    HELP,
+    SETTING,
+    WATERTANK,
+    SHARKSHOP,
+    COSTBAR
+};
