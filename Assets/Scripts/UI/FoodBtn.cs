0ec32c007bec5a7f3be3e3e15aeb0dafa0a6dafd
hansangsu <hsangsu40@gmail.com>
2021-01-28 17:31:24 +0900

    푸시

---
+++ Assets/Scripts/UI/FoodBtn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodBtn : MonoBehaviour
+{
+    string key;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ClickBtn);
+    }
+
+    void ClickBtn()
+    {
+        key = GetComponent<ScrollItem>().itemname.text;
+        FoodMng.Instance.key=key;
+    }
+}
