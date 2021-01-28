432ab6452128c2878c13a96de0ab0d58aac10ba6
hwanjuson <hwanjuson@gmail.com>
2021-01-28 10:56:43 +0900

    음..

--- Assets/Scripts/SharkPlus.cs
+++ Assets/Scripts/SharkPlus.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SharkPlus : MonoBehaviour
-{  
+{
+    [SerializeField]
+    private Text sharkText;
+
+    public int _count;
+    public int _index;
+
+    _SharkData _sharkData;
+    private void Start()
+    {
+        _count = 0;
+        _index = 0;
+    }
+
     public bool Shark_Plus(int index, _SharkData data, int count)
     {
         if(GameManager.Instance.watertank[index].sharks.Count == 0) //수조에 상어가 없다면
@@ -29,5 +43,44 @@ public class SharkPlus : MonoBehaviour
         {
             return false;
         }
+    }
+    public void Buy() //테스트 안해봄...되는지는 모르겠어요 - 아직 버튼연동 X
+    {
+        if(GameManager.Instance.money.money > _sharkData.price)
+        {
+            GameManager.Instance.money.SubMoney(_count * _sharkData.price);
+            GameManager.Instance.money.MoneyUpdate();
+            Shark_Plus(_index, _sharkData, _count);
+        }
+        else if(GameManager.Instance.money.money < _sharkData.price)
+        {
+            //돈 부족
+            return;
+        }
+    }
+
+    public void Text_Plus()
+    {
+        _count++;
+        sharkText.text = _count + "x";
+    }
+
+    public void Text_Minus()
+    {
+        if (_count > 0)
+        {
+            _count--;
+            sharkText.text = _count + "x";
+        }
+    }
+
+    public void Index_Zero()
+    {
+        _index = 0;
+    }
+
+    public void Index_One()
+    {
+        _index = 1;
     }
 }
